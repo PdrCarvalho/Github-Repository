37aa9411f22e71623014f9157822010632682361
carvalho <carvalho.pedro.cardoso@gmail.com>
2020-07-03 12:10:48 -0700

    ready test environment

---
+++ backend/__test__/archived.test.js
@@ -0,0 +1,18 @@
+
+describe('Archivedm model Test', () => {
+    beforeAll(  () => {
+    
+    });
+   
+    it('create & save archived successfully', async () => {
+        expect(true).toBe(true); 
+    });
+    
+    afterAll(()=>{
+       
+    })
+
+  
+
+    
+})
